42f480d2d1cff3dbc24667025613ced914a2bae6
Reema <Reemaalshohof88@gmail.com>
2022-05-09 12:01:05 +0200

    add feedbackItem component, add globale styles

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
+  );
+};
+
+export default App;
