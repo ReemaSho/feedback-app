4489319962bcda49cdeb5ed9e1af826596a22e48
Reema <Reemaalshohof88@gmail.com>
2022-05-09 12:08:14 +0200

    add feedback data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/FeedbackData";
 const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
