c68a6b46b963bd19e4b7174eecdb142a3f76d1dd
Reema <Reemaalshohof88@gmail.com>
2022-05-10 08:54:31 +0200

    add form component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
+
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedback = (id) => {
@@ -14,6 +16,7 @@ const App = () => {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
